c4f894e0e4777bbed081cf7e0790ce3fec259f8c
azbagas <azharbagaskara1@gmail.com>
2023-07-01 19:06:53 +0700

    add pertemuan 17

---
+++ pertemuan17/index.php
@@ -0,0 +1,74 @@
+<?php
+    session_start();
+    if (!isset($_SESSION["login"])) {
+        header("Location: login.php");
+        exit;
+    }
+
+    require 'functions.php'; // Hubungin file
+    $students = query("SELECT * FROM mahasiswa ORDER BY id DESC");
+
+    // Kalo nyari
+    if (isset($_POST["cari"])) {
+        $students = cari($_POST["keyword"]);
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+<body>
+    <a href="logout.php">Logout</a>
+
+    <h1>Daftar Mahasiswa</h1>
+
+    <a href="tambah.php">Tambah data mahasiswa</a>
+    <br><br>
+
+    <form action="" method="post">
+        <input type="text" name="keyword" size="40" autofocus
+        placeholder="keyword pencarian..." autocomplete="off">
+        <button type="submit" name="cari">Cari!</button>
+    </form>
+    <br>
+
+    <table border="1" cellpadding="10" cellspacing="0">
+        <thead>
+            <tr>
+                <th>No.</th>
+                <th>Aksi</th>
+                <th>Gambar</th>
+                <th>NIM</th>
+                <th>Nama</th>
+                <th>Email</th>
+                <th>Jurusan</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php $i = 1; ?>
+            <?php foreach ($students as $student) : ?>
+                <tr>
+                    <td><?= $i; ?></td>
+                    <td>
+                        <a href="ubah.php?id=<?= $student["id"]; ?>">ubah</a> |
+                        <a href="hapus.php?id=<?= $student["id"]; ?>" 
+                            onclick="return confirm('Yakin ingin menghapus <?= $student["nama"]; ?>?');">
+                            hapus
+                        </a>
+                    </td>
+                    <td><img src="img/<?= $student["gambar"]; ?>" width="50"></td>
+                    <td><?= $student["nim"]; ?></td>
+                    <td><?= $student["nama"]; ?></td>
+                    <td><?= $student["email"]; ?></td>
+                    <td><?= $student["jurusan"]; ?></td>
+                </tr>
+                <?php $i++; ?>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</body>
+</html>
